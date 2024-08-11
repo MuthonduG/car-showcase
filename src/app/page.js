73ce49b5899988ecbe2b081e70e90a1bbdbacbcb
MuthonduG <muthondugithinji@gmail.com>
2024-08-11 20:33:26 +0300

    initial commit for header logic which includes a nav component

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,28 @@
-import Image from "next/image";
+// Components
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Cars from "./components/Cars";
+import About from "./components/About";
+import Why from "./components/Why";
+import Testimonials from "./components/Testimonials";
+import Cta from "./components/Cta";
+import Footer from "./components/Footer";
+import ToTopBtn from "./components/ToTopBtn";
 
 export default function Home() {
   return (
-    <main className=""> main   </main>
+    <main className="max-w-[1920px] bg-white mx-auto relative overflow-hidden">
+      <Header/>
+      <Hero/>
+      <Cars/>
+      <About/>
+      <Why/>
+      <Testimonials/>
+      <Cta/>
+      <Footer/>
+      <ToTopBtn/>
+
+      <div className="h-[4000px]"></div>
+    </main>
   );
 }
