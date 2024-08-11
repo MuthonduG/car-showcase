4cdffc6da7782a3a96fb006fbd39d32cc54529c9
MuthonduG <muthondugithinji@gmail.com>
2024-08-11 14:28:46 +0300

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,27 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '15px'
+      }
+    },
+    screens: {
+      sm: '640px',
+      md: '760px',
+      lg: '1024px',
+      xl: '1300px'
+    },
     extend: {
+      colors: {
+        primary: '#101828',
+        secondary: '#667085',
+        accesnt: {
+          DEFAULT: '#ed1d24',
+          hover: '#dd242a'
+        },
+        body: '#dedede'
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
