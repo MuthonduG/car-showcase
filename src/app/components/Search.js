9509a06273daa84d1970f45b0531658e9306ea51
MuthonduG <muthondugithinji@gmail.com>
2024-08-13 03:51:11 +0300

    initial commit for Dateselection and update to global.css

--- src/app/components/Search.js
+++ src/app/components/Search.js
@@ -2,6 +2,7 @@
 import { useContext } from "react"
 import { SearchContext } from "../context/search"
 import Locationselection from "./Locationselection"
+import Dateselection from "./Dateselection"
 
 export default function Search() {
   const {searchActive} = useContext(SearchContext)
@@ -9,6 +10,7 @@ export default function Search() {
     <div className={`${searchActive ? 'bg-white rounded-none xl:h-[80px]' : 'bg-white rounded-[20px] py-6 xl:pr-4 xl:h-[98px]'} hidden xl:block w-full relative shadow-lg`}>
       <div className="xl:h-full flex items-center px-6 xl:px-0">
         <Locationselection/>
+        <Dateselection/>
       </div>
     </div>
   )
