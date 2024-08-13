89836e80b0833f63a995d3c1cb0f35c9c84e3da4
MuthonduG <muthondugithinji@gmail.com>
2024-08-13 23:06:24 +0300

    update commit for Brands: added car logos icons

--- src/app/components/Cars.js
+++ src/app/components/Cars.js
@@ -1,7 +1,14 @@
+import Brands from "./Brands";
+import Carslider from "./Carslider";
+
+
 export default function Cars() {
   return (
-    <section className="h-screen flex items-center bg-yellow-200" id="cars">
-      Cars
+    <section className="h-screen flex items-center" id="cars">
+      <div className="container mx-auto">
+        <Brands/>
+        <Carslider/>
+      </div>
     </section>
   )
 }
