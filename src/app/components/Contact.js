8423c2012d278007a0f263c006afe1ea1391787c
MuthonduG <muthondugithinji@gmail.com>
2024-08-16 15:08:47 +0300

    final commit

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -5,7 +5,7 @@ import { fadeIn } from "/variants";
 
 export default function Cta() {
   return (
-      <section className="pt-24 xl:pt-48 items-end pb-0" id="contact">
+      <section className="pt-24 xl:pt-48 items-end pb-0 overflow-hidden" id="contact">
         <div className="container mx-auto">
           <div className="flex flex-col md:flex-row xl:items-center">
             <div className="flex-1 xl:ml-24 text-center md:text-left mb-12 md:mb-0">
@@ -53,7 +53,7 @@ export default function Cta() {
               </div>
             </div>
             <motion.div
-              variants={fadeIn('up', 0.8)}
+              variants={fadeIn('left', 0.8)}
               initial="hidden"
               whileInView={'show'}
               viewport={{ once: false, amount: 0.2 }}
